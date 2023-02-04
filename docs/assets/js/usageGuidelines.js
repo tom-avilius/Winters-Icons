ed2002f8921a514d64edd4f0edaf2fc63fc12b2e
tomriddle <aaarshdwivedi244475@gmail.com>
2023-02-04 14:11:53 +0530

    Added not allowed section to usageGuidelines.html file along with behaviour.

--- docs/assets/js/usageGuidelines.js
+++ docs/assets/js/usageGuidelines.js
@@ -2,8 +2,10 @@
 // getting html elements
 const introContent = document.getElementById('intro-div');
 const allowedContent = document.getElementById('allowed-div');
+const notAllowedContent = document.getElementById('not-allowed-div');
 const introButton = document.getElementById('intro');
 const allowedButton = document.getElementById('allowed');
+const notAllowedButton = document.getElementById('not-allowed')
 
 // adding click behaviour to navbar
 introButton.addEventListener('click', () => {
@@ -11,6 +13,7 @@ introButton.addEventListener('click', () => {
     introContent.classList.remove('hidden');
 
     allowedContent.classList.add('hidden');
+    notAllowedContent.classList.add('hidden')
 });
 
 allowedButton.addEventListener('click', () => {
@@ -18,4 +21,13 @@ allowedButton.addEventListener('click', () => {
     allowedContent.classList.remove('hidden');
 
     introContent.classList.add('hidden');
-})
+    notAllowedContent.classList.add('hidden');
+});
+
+notAllowedButton.addEventListener('click', () => {
+
+    notAllowedContent.classList.remove('hidden');
+
+    introContent.classList.add('hidden');
+    allowedContent.classList.add('hidden');
+});
